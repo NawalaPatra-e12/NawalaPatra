3cda3c382014957161928593ecdec2114a5887a7
NaufalAul <auliactivity@gmail.com>
2023-10-27 10:21:24 +0700

    aul-add: bookmarked module test

--- mybooks/views.py
+++ mybooks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from main.models import UserProfile, User
+from library.models import Book
+
 
 # Create your views here.
+@login_required(login_url='/login/')
+# def show_mybooks(request):
+
+#     return render(request, "mybooks.html")
+
+def show_mybooks(request):
+    user_profile = request.user.userprofile
+    bookmarked_books = user_profile.bookmarked_books.all()
+    return render(request, 'mybooks.html', {'bookmarked_books': bookmarked_books})
