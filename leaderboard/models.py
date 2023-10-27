c3af8034885b6c7d81a01d296e4a7376681d3bc0
Natanael Bonaparte <natanael.bonaparte@ui.ac.id>
2023-10-27 11:13:58 +0700

    natan:1st commit leaderboard

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    image_url = models.URLField()
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    rate = models.IntegerField(default=0)
