0fa9757de0d7c36faac93fa09a9352238355601b
AzkaNydiaEsti <azka.nydia@ui.ac.id>
2023-10-28 21:14:57 +0700

    azkan: writersjam update 3

--- writersjam/models.py
+++ writersjam/models.py
@@ -7,24 +7,9 @@ class Prompt(models.Model):
     week = models.IntegerField()
     genre = models.CharField(max_length=200)
 
-    # THEMES = [
-    #     ("Literature & Fiction", "Another world fantasy"),
-    #     ("Mystery, Thriller & Suspense", "Haloween horror time"),
-    #     ("Religion & Spirituality", "About a mysterious ritual"),
-    #     ("Romance", "Mafia Romance"),
-    #     ("Science Fiction & Fantasy", "Abnormal dimension gate"),
-    # ]
-
-    # GENRES = [
-    #     ("Literature & Fiction", "Literature & Fiction"),
-    #     ("Mystery, Thriller & Suspense", "Mystery, Thriller & Suspense"),
-    #     ("Religion & Spirituality", "Religion & Spirituality"),
-    #     ("Romance", "Romance"),
-    #     ("Science Fiction & Fantasy", "Science Fiction & Fantasy"),
-    # ]
-
 class Submission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    username = models.CharField(max_length=150, null=True)
     date = models.DateField(null=True)
     title = models.CharField(max_length=100)
     story = models.TextField()
