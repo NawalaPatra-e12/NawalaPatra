c43942f37b7ee5e2d5de4609021b010185514657
jep <n.siomusic@gmail.com>
2023-10-29 10:06:13 +0700

    almap-add: request bulletin

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from library.views import show_library, bookmark_book
+from library.views import show_library, bookmark_book, show_requests
 from library.views import search_products, filter_category
 from library.views import show_json, show_json_by_id, show_xml, show_xml_by_id
 from library.views import get_request_json, add_request_ajax
@@ -17,4 +17,5 @@ urlpatterns = [
     path('bookmark_book/', bookmark_book, name='bookmark_book'),
     path('get-request/', get_request_json, name='get_request_json'),
     path('create-request-ajax/', add_request_ajax, name='add_request_ajax'),
+    path ('requests/', show_requests, name='show_requests'),
 ]
