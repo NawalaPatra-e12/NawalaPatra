027053643e6c47762491d6466513389bb2b8b253
Natanael Bonaparte <natanael.bonaparte@ui.ac.id>
2023-12-17 12:46:18 +0700

    natan: add smth to leaderboard

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -6,9 +6,12 @@ app_name = 'leaderboard'
 urlpatterns = [
     path('',show_leaderboard,name='show_leaderboard'),
     path('filter/<int:id>/', filter_leaderboard, name='filter_leaderboard'),
+    path('filter-json/<int:id>/', filter_id_json, name='filter_id_json'),
     path('rate_button',rate_button,name='rate_button'),
     path('get_book_json',get_book_json,name='get_book_json'),
     path('add_comment', add_comment, name = 'add_comment'),
     path('get_comment_json', get_comment_json, name='get_comment_json'),
     path('comments/',show_comment, name="show_comment"),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('json', show_json, name='show_json'),
     ]
